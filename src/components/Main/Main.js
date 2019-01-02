b72bfcc6309eefc1e6d13e31eea9e2e48dbae2e2
Ryan Mundy <ryanmundy34@gmail.com>
2019-01-02 16:28:08 -0600

    adds styling to main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-// import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+// import CardContent from '@material-ui/core/CardContent';
+// import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import './Main.css';
+
 
 class Main extends Component {
     componentDidMount() {
@@ -18,40 +19,32 @@ class Main extends Component {
 
     render() {
 
+        let cardStyle = {
+            width: 300
+        }
+
         let featuredProduct = this.props.reduxStore.featured.map(product => {
             return (
-                <Card id="product" key={product.id}>
-                    {/* <h3>{product.name}</h3>
-                    <img src={product.image_url} width="300" alt=''></img> */}
-                    <CardActionArea>
-                        <h1>Featured Product</h1>
-                        <h2>{product.name}</h2>
-                        <img src={product.image_url} height="300" alt=''></img>
-                        <CardContent>
-
-                            <Typography variant="h5" component="p">
-                                {product.description}
-                            </Typography>
-                            <Typography component="p">
-                                <em>Added by: {product.username}</em>
-                            </Typography>
-                        </CardContent>
-                    </CardActionArea>
+                <Card style={cardStyle} id="product" key={product.id}>
+                    <h1>Featured Product</h1>
+                    <h2>{product.name}</h2>
+                    <h3>Rating: {product.rating}</h3>
+                    <p><em>Added By: {product.username}</em></p>
+                    <img src={product.image_url} height="300" alt=''></img>
                 </Card>
             );
         })
-        
+
         return (
             <div>
-            <div>
-                <h1>Main Page</h1>
-                {JSON.stringify(this.props.reduxStore.featured[0])}
-            </div>
-            <div>
-                    <Card>
-                       {featuredProduct}
-                    </Card>
-            </div>
+                <div id="welcome">
+                    <h1><em>Welcome to the page of all things caffeine!</em></h1>
+                    <h4><em>Login or sign up for a free account to begin sharing your favorite products!</em></h4>
+                    {/* {JSON.stringify(this.props.reduxStore.featured[0])} */}
+                </div>
+                <div>
+                    {featuredProduct}
+                </div>
             </div>
         )
     }
