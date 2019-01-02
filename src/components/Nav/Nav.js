342a116eef8abd75e6808238442e1c10786669da
Ryan Mundy <ryanmundy34@gmail.com>
2019-01-02 12:25:34 -0600

    adds nav buttons

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -7,7 +7,7 @@ import './Nav.css';
 const Nav = (props) => (
   <div className="nav">
     <Link to="/home">
-      <h2 className="nav-title">Prime Solo Project</h2>
+      <h2 className="nav-title">caffeinated</h2>
     </Link>
     <div className="nav-right">
       <Link className="nav-link" to="/home">
@@ -17,18 +17,25 @@ const Nav = (props) => (
         {props.user.id ? 'Home' : 'Login / Register'}
       </Link>
       {/* Show the link to the info page and the logout button if the user is logged in */}
-      {props.user.id && (
-        <>
-          <Link className="nav-link" to="/info">
-            Info Page
-          </Link>
-          <LogOutButton className="nav-link"/>
-        </>
-      )}
+     
       {/* Always show this link since the about page is not protected */}
+      <Link className="nav-link" to="/products">
+        Products
+      </Link>
+      <Link className="nav-link" to="/locate">
+        Locate
+      </Link>
       <Link className="nav-link" to="/about">
         About
       </Link>
+      {props.user.id && (
+        <>
+          {/* <Link className="nav-link" to="/info">
+            Info Page
+          </Link> */}
+          <LogOutButton className="nav-link" />
+        </>
+      )}
     </div>
   </div>
 );
