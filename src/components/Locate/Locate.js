fc4bf7d0b149ed6f9c2ca1f0918fbcc3a8c905e6
Ryan Mundy <ryanmundy34@gmail.com>
2019-01-09 16:23:04 -0600

    map displays on DOM

--- src/components/Locate/Locate.js
+++ src/components/Locate/Locate.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 
 class Locate extends Component {
     componentDidMount() {
 
     }
 
+    
+
     render() {
+        const style = {
+            width: '100%',
+            height: '100%'
+        }
         return (
             <div>
                 <h1>Map goes here</h1>
+                <Map style={style} google={this.props.google} zoom={14}
+                    initialCenter={{
+                        lat: 44.9778,
+                        lng: -93.2650
+                    }}>
+                    
+                    <Marker onClick={this.onMarkerClick}
+                        name={'Current location'} />
+                    <InfoWindow onClose={this.onInfoWindowClose}>
+                    </InfoWindow>
+                </Map>
             </div>
         )
     }
 }
 
-export default connect()(Locate);
+// export default connect()(Locate);
+
+export default GoogleApiWrapper({
+    apiKey: ('')
+})(Locate)
