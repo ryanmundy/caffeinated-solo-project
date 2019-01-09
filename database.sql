30ae41c3d0cd55df51be456076f85a0389d5bb18
Ryan Mundy <ryanmundy34@gmail.com>
2019-01-08 21:00:24 -0600

    updates sql file

--- database.sql
+++ database.sql
@@ -1,49 +1,55 @@
-CREATE TABLE "person"
+CREATE TABLE person
 (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL,
-    "admin" BOOLEAN default false
+    id SERIAL PRIMARY KEY,
+    username character varying(80) NOT NULL UNIQUE,
+    password character varying(1000) NOT NULL,
+    admin boolean DEFAULT false
 );
 
-CREATE TABLE "reviews"
+CREATE TABLE reviews
 (
-    "id" serial primary key NOT NULL,
-    "review_content" varchar(5000) NOT NULL,
-    "product_id" int NOT NULL references "products"
-);
-
-
-CREATE TABLE "categories"
-(
-    "id" serial primary key NOT NULL,
-    "category" varchar(100) NOT NULL
+    id SERIAL PRIMARY KEY,
+    review_content character varying(5000) NOT NULL,
+    product_id integer NOT NULL REFERENCES products(product_table_id) ON DELETE CASCADE,
+    rating integer NOT NULL
 );
 
 
-CREATE TABLE "location"
+CREATE TABLE categories
 (
-    "id" serial primary key NOT NULL,
-    "street_number" int NOT NULL,
-    "street_name" varchar(500) NOT NULL,
-    "city" varchar(500) NOT NULL,
-    "state" varchar(500) NOT NULL,
-    "zip" int NOT NULL,
-    "product_id" int references "products" NOT NULL
+    id SERIAL PRIMARY KEY,
+    category character varying(100) NOT NULL
 );
 
 
-CREATE TABLE "products"
+CREATE TABLE location
 (
-    "id" serial primary key NOT NULL,
-    "name" varchar(100) NOT NULL,
-    "description" varchar(5000),
-    "caffeine_content" int,
-    "rating" int,
-    "image_url" varchar(5000),
-    "featured" BOOLEAN default false NOT NULL,
-    "added_by" int NOT NULL references "person",
-    "category_id" int NOT NULL references "categories"
+    id SERIAL PRIMARY KEY,
+    street_number integer NOT NULL,
+    street_name character varying(500) NOT NULL,
+    city character varying(500) NOT NULL,
+    state character varying(500) NOT NULL,
+    zip integer NOT NULL,
+    product_id integer NOT NULL REFERENCES products(product_table_id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE products
+(
+    product_table_id integer DEFAULT nextval('products_id_seq'
+    ::regclass) PRIMARY KEY,
+    name character varying
+    (100) NOT NULL,
+    description character varying
+    (5000),
+    caffeine_content integer,
+    image_url character varying
+    (5000),
+    featured boolean NOT NULL DEFAULT false,
+    added_by integer NOT NULL REFERENCES person
+    (id),
+    category_id integer NOT NULL REFERENCES categories
+    (id)
 );
 
 --See all products
