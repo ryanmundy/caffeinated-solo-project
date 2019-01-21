46672fb0879695655d540bb833f7725e470c64a8
Ryan Mundy <ryanmundy34@gmail.com>
2019-01-21 15:04:56 -0600

    updates readme and sql file

--- database.sql
+++ database.sql
@@ -6,6 +6,7 @@ CREATE TABLE person
     admin boolean DEFAULT false
 );
 
+
 CREATE TABLE reviews
 (
     id SERIAL PRIMARY KEY,
@@ -15,6 +16,7 @@ CREATE TABLE reviews
 );
 
 
+
 CREATE TABLE categories
 (
     id SERIAL PRIMARY KEY,
@@ -22,16 +24,20 @@ CREATE TABLE categories
 );
 
 
+
 CREATE TABLE location
 (
     id SERIAL PRIMARY KEY,
-    street_number integer NOT NULL,
-    street_name character varying(500) NOT NULL,
+    store character varying(1000) NOT NULL,
+    street_address character varying(1000) NOT NULL,
     city character varying(500) NOT NULL,
     state character varying(500) NOT NULL,
     zip integer NOT NULL,
-    product_id integer NOT NULL REFERENCES products(product_table_id) ON DELETE CASCADE
+    lat character varying(500),
+    lng character varying(500),
+    product_id integer REFERENCES products(product_table_id) ON DELETE CASCADE
 );
+
 
 
 CREATE TABLE products
@@ -51,37 +57,3 @@ CREATE TABLE products
     category_id integer NOT NULL REFERENCES categories
     (id)
 );
-
---See all products
-SELECT *
-FROM "products"
-ORDER BY "products".name ASC;
-
---See reviews and rating
-SELECT "products".name, "products".rating, "reviews".review_content
-FROM "products"
-    JOIN "reviews" ON "products".id = "reviews".product_id
-ORDER BY "products".name ASC;
-
---See who added products
-SELECT "products".name, "person".username
-FROM "products"
-    JOIN "person" ON "products".added_by = "person".id
-ORDER BY "person".username ASC;
-
---See admin
-SELECT *
-FROM "person"
-WHERE "person".admin=true;
-
---See featured product
-SELECT *
-FROM "products"
-WHERE "products".featured=true;
-
---See locations
-SELECT "products".name, "location".street_number, "location".street_name, "location".city,
-    "location".state, "location".zip
-FROM "products"
-    JOIN "location" ON "products".id = "location".product_id
-ORDER BY "products".name ASC;
